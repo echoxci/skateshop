b1b8894efd114b4f46111257fcd5f1940e4ef4d3
echoxci <frederik.veres.tr@gmail.com>
2022-01-08 01:51:08 +0100

    created admin middleware

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -21,12 +21,14 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected function authenticated()
+    {
+        if(Auth::user()->role_as == '1') {
+            return redirect('dashboard')->with('status','Welcome to your dashboard');
+        }
+
+        return redirect('/')->with('status','Logged in successfully');
+    }
 
     /**
      * Create a new controller instance.
