2e465e1400f7ef05a27e5f7020f92f5b66af377e
echoxci <frederik.veres.tr@gmail.com>
2022-01-12 12:50:44 +0100

    Added edit/update category with images

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class CategoryController extends Controller
 {
     public function index() {
-        return view('admin.category.index');
+        $category = Category::all();
+        return view('admin.category.index', compact('category'));
     }
 
     public function add() {
@@ -35,5 +37,35 @@ class CategoryController extends Controller
         $category->meta_description = $request->input('meta_description');
         $category->save();
         return redirect('/dashboard')->with('status', "Category Added Successfully");
+    }
+
+    public function edit($id) {
+        $category = Category::find($id);
+        return view('admin.category.edit', compact('category'));
+    }
+
+    public function update(Request $request, $id) {
+        $category = Category::find($id);
+        if ($request->hasFile('image')) {
+            $path = 'assets/uploads/category/'.$category->image;
+            if (File::exists($path)) {
+                File::delete($path);
+            }
+            $file = $request->file('image');
+            $ext = $file->getClientOriginalExtension();
+            $fileName = time().'.'.$ext;
+            $file->move('assets/uploads/category/', $fileName);
+            $category->image = $fileName;
+        }
+        $category->name = $request->input('name');
+        $category->slug = $request->input('slug');
+        $category->description = $request->input('description');
+        $category->status = $request->input('status') == TRUE ? '1':'0';
+        $category->popular = $request->input('popular') == TRUE ? '1':'0';
+        $category->meta_title = $request->input('meta_title');
+        $category->meta_keywords = $request->input('meta_keywords');
+        $category->meta_description = $request->input('meta_description');
+        $category->update();
+        return redirect('dashboard')->with('status',"Category Updated Successfully");
     }
 }
--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -2,10 +2,38 @@
 
 @section('content')
     <div class="card">
+        <div class="card-header">
+            <h4>Kategorie</h4>
+            <hr>
+        </div>
         <div class="card-body">
-            <h1>
-                Kategorie
-            </h1>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Image</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach($category as $item)
+                    <tr>
+                        <td>{{ $item->id }}</td>
+                        <td>{{ $item->name }}</td>
+                        <td>{{ $item->description }}</td>
+                        <td>
+                            <img src="{{ asset('assets/uploads/category/'.$item->image) }}" class="category-image" alt="Image here">
+                        </td>
+                        <td>
+                            <a href="{{url('edit-prod/'.$item->id)}}" class="btn btn-primary">Edit</a>
+                            <a href="{{url('delete-prod/'.$item->id)}}" class="btn btn-danger">Delete</a>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 @endsection
